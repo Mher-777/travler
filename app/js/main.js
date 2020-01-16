435b6d11d1529a2a5d9c475dccc4d5156238bac2
Mher-777 <mherjan2003@mail.ru>
2020-01-16 21:59:56 +0400

    added section main-info

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,22 @@
-$(function(){
+$(function () {
 
     $('.traveling-slider').slick({
         arrows: false,
         autoplay: true,
-    })
+    });
+
     $('.team__slider').slick({
         arrows: false,
         autoplay: true,
         dots: true,
         dotsClass: 'team__slider-dots',
+    });
+
+    $('.main-info__item-btn').on('click', function () {
+        $(".main-info__item:nth-child(1)").remove(),
+            $(".main-info__item").removeClass('.main-info__item-remove')
     })
-  
+    $('.main-info__item-btn').on('click', function () {
+        $(".main-info__item-remove").removeClass('main-info__item-remove')
+    })
 });
