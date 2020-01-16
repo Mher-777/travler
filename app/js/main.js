a5366cbaaffa458530976c13333ea80aea748858
Mher-777 <mherjan2003@mail.ru>
2020-01-16 20:52:11 +0400

    added section team

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,11 @@ $(function(){
         arrows: false,
         autoplay: true,
     })
+    $('.team__slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        dotsClass: 'team__slider-dots',
+    })
   
 });
